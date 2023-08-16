5abc9d8be634c548ca9b119e5f6bd144d5d37fd4
BinhMinhPTIT <binhminhtt03@gmail.com>
2023-08-16 11:00:10 +0700

    Add new project

---
+++ public/js/api.js
@@ -0,0 +1,11 @@
+let api_key = "6ff5936f4347b74431a890d6ccc20eb3";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let original_img_url = "https://image.tmdb.org/t/p/original";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
